f6a98b4f1a550e68dff09806607011bf8f355126
Neo Ighodaro <neo@creativitykills.co>
2018-01-11 16:35:43 +0100

    Updated

--- words/AppDelegate.swift
+++ words/AppDelegate.swift
@@ -11,8 +11,8 @@ import UIKit
 struct AppConstants {
     static let ENDPOINT = "http://127.0.0.1:8000"
     static let CLIENT_ID = 2
-    static let CLIENT_SECRET = "iQlyOZoA7RPXvFaZBM1iKIFXeu0Hq855a2NV7ERt"
-    static let CHATKIT_INSTANCE_ID = "v1:us1:44cc778a-492d-4000-ab1a-abaac05e02fa"
+    static let CLIENT_SECRET = "CLIENT_SECRET"
+    static let CHATKIT_INSTANCE_ID = "INSTANCE_ID"
 }
 
 @UIApplicationMain
@@ -21,9 +21,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        let ud = UserDefaults.standard
-        ud.removePersistentDomain(forName: Bundle.main.bundleIdentifier!)
-        ud.synchronize()
         return true
     }
 }
